aca45f48e938fca54876d04521fd00401728c0ff
ctdk <jbingham@gmail.com>
2015-03-01 22:08:20 -0500

    wayward space got deleted in the stop_dev.sh script

--- stop_dev.sh
+++ stop_dev.sh
@@ -4,7 +4,7 @@ docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
 screen -X -S raintank quit
 echo "Stopped docker dev environment"
 
-if [ ! -z $1]; then
+if [ ! -z $1 ]; then
 	if [ $1 == "destroy" ]; then
 		docker rmi $(docker images -q)
 	fi
