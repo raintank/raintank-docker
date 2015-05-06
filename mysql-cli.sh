270ed6512ed8a59d812d2dd1e4d660571687489b
Dieter Plaetinck <dieter@raintank.io>
2015-05-06 08:52:23 -0400

    simpler approach: just use mysql in container and open in screen session

--- mysql-cli.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-container=$(docker ps | awk '/raintankdocker_mysql_1/ {print $1}')
-if [ -z "$container" ]; then
-    echo "no running raintank mysql container found" >&2
-    exit 1
-fi
-port=$(docker inspect -f "{{ .NetworkSettings.Ports }}" $container | sed 's#.*:\([0-9]\+\).*#\1#')
-exec mysql -h 127.0.0.1 -P $port -D grafana -u grafana -ppassword
