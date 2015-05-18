c9b582aa0bd617a4cd2dc8c887b65dbe54934c44
Dieter Plaetinck <dieter@raintank.io>
2015-05-18 11:49:52 -0400

    update stop_dev script. fix #7

--- stop_dev.sh
+++ stop_dev.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
 screen -X -S raintank quit
+docker-compose -f fig-dev.yaml stop
+yes | docker-compose -f fig-dev.yaml rm
 echo "Stopped docker dev environment"
-
-if [ ! -z $1 ]; then
-	if [ $1 == "destroy" ]; then
-		docker rmi $(docker images -q)
-	fi
-	echo "Destroyed docker images"
-fi
