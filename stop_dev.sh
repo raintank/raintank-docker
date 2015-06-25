e9fb26199a35d430a7159ec1efc756dcc085d753
Dieter Plaetinck <dieter@raintank.io>
2015-06-25 15:22:45 -0700

    also remove already stopped collectors, preventing name conflicts

--- stop_dev.sh
+++ stop_dev.sh
@@ -7,6 +7,10 @@ yes | docker-compose -f fig-dev.yaml rm
 # stop other collectors that were started, if any
 for id in $(docker ps | grep raintankdocker_raintankCollector | cut -d' ' -f1); do
   docker stop $id
+done
+
+# assure no collectors images occupying the name, which can happen by stopped containers
+for id in $(docker ps -a | grep raintankdocker_raintankCollector | cut -d' ' -f1); do
   docker rm $id
 done
 
