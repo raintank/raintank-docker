b9a9d8ecc6b403e74c56fc2a1632e36d9a7c383e
Dieter Plaetinck <dieter@raintank.io>
2015-07-14 09:54:41 -0700

    fix influxdb datasource definition

--- grafana-dev/create-influxdb-dev-datasource.sh
+++ grafana-dev/create-influxdb-dev-datasource.sh
@@ -17,4 +17,4 @@ echo "> adding datasource"
 
 curl -H "Authorization: Bearer eyJrIjoiMEVUVE52c3ZITnhpVldyOTI5cVFQcUxQWGR6V213bUIiLCJuIjoiZGV2c3RhY2stYWRtaW4iLCJpZCI6MX0=" \
   -H "content-type: application/json" \
-  'http://localhost/api/datasources' -X POST --data-binary '{"name":"influxdb","type":"influxdb_08","url":"http://influxdb:8086","access":"proxy","isDefault":false,"database":"raintank","user":"graphite","password":"graphite"}'
+  'http://localhost/api/datasources' -X POST --data-binary '{"name":"influxdb","type":"influxdb_08","url":"http://localhost:8086","access":"direct","isDefault":false,"database":"raintank","user":"graphite","password":"graphite"}'
