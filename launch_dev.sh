d8edc1f295480900d714ded887d9fb428f88eb45
Dieter Plaetinck <dieter@raintank.io>
2015-08-12 09:46:26 +0200

    initial work to nsq based metrics pipeline instead of go-metrics
    (though it uses the raintank-metrics repo for code)

--- launch_dev.sh
+++ launch_dev.sh
@@ -36,8 +36,11 @@ screen -S raintank -X screen -t statsdaemon docker exec -t -i raintankdocker_sta
 screen -S raintank -X screen -t influxdb docker exec -t -i raintankdocker_influxdb_1 bash
 
 
-#raintank-metric - this app consumes the metric data written to the message queue and sends it to influxdb.  The app also performs threshold checking and data roll-ups
-screen -S raintank -X screen -t metric docker exec -t -i raintankdocker_raintankMetric_1 bash
+#nsqtokairos
+screen -S raintank -X screen -t nsqtokairos docker exec -t -i raintankdocker_nsqtokairos_1 bash
+
+#nsqtoelasticsearch
+screen -S raintank -X screen -t nsqtoelasticsearch docker exec -t -i raintankdocker_nsqtoelasticsearch_1 bash
 
 # open a mysql cli for convenience
 screen -S raintank -X screen -t mysql-cli docker exec -t -i $(docker ps | awk '/raintankdocker_mysql_1/ {print $1}') mysql -prootpass grafana
@@ -47,7 +50,10 @@ screen -S raintank -p graphite-api -X stuff 'tail -10f /var/log/raintank/graphit
 screen -S raintank -p grafana -X stuff '/tmp/create-influxdb-dev-datasource.sh &> /var/log/raintank/create-influxdb-datasource.log; touch /var/log/raintank/grafana-dev.log\n'
 screen -S raintank -p grafana -X stuff 'cat /var/log/raintank/create-influxdb-datasource.log\n'
 screen -S raintank -p grafana -X stuff 'tail -10f /var/log/raintank/grafana-dev.log\n'
-screen -S raintank -p metric -X stuff 'tail -10f /var/log/raintank/metric.log\n'
+screen -S raintank -p nsqtokairos -X stuff 'cd /go/src/github.com/raintank/raintank-metric/nsq_to_kairos\n'
+screen -S raintank -p nsqtokairos -X stuff './nsq_to_kairos --topic metrics --channel tokairos --nsqd-tcp-address nsqd:4150\n'
+screen -S raintank -p nsqtoelasticsearch -X stuff 'cd /go/src/github.com/raintank/raintank-metric/nsq_to_elasticsearch\n'
+screen -S raintank -p nsqtoelasticsearch -X stuff './nsq_to_elasticsearch --topic metrics --channel toelasticsearch --nsqd-tcp-address nsqd:4150\n'
 screen -S raintank -p statsdaemon -X stuff 'tail -f /var/log/statsdaemon.log\n'
 screen -S raintank -p influxdb -X stuff 'tail -f /opt/influxdb/shared/log.txt\n'
 
