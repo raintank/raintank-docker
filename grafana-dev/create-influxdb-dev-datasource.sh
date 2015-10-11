1f13abb842a4f774e7d5d0b4bb783686a29e3648
Dieter Plaetinck <dieter@raintank.io>
2015-10-11 16:35:48 +0000

    automatically add alerting dashboard
    from
    https://github.com/raintank/grafana/commit/9442008caf0e8e266922c5506ce62aa4d5091231
    
    fixes #27

--- grafana-dev/create-influxdb-dev-datasource.sh
+++ grafana-dev/create-influxdb-dev-datasource.sh
@@ -18,3 +18,10 @@ echo "> adding datasource"
 curl -H "Authorization: Bearer eyJrIjoiMEVUVE52c3ZITnhpVldyOTI5cVFQcUxQWGR6V213bUIiLCJuIjoiZGV2c3RhY2stYWRtaW4iLCJpZCI6MX0=" \
   -H "content-type: application/json" \
   'http://localhost/api/datasources' -X POST --data-binary '{"name":"influxdb","type":"influxdb_08","url":"http://localhost:8086","access":"direct","isDefault":false,"database":"raintank","user":"graphite","password":"graphite"}'
+
+for file in /tmp/dashboards/*; do
+  echo "> adding dashboard $file"
+  curl -H "Authorization: Bearer eyJrIjoiMEVUVE52c3ZITnhpVldyOTI5cVFQcUxQWGR6V213bUIiLCJuIjoiZGV2c3RhY2stYWRtaW4iLCJpZCI6MX0=" \
+    -H "content-type: application/json" \
+    'http://localhost/api/dashboards/db' -X POST -d "{\"dashboard\": $(cat $file)}"
+done
