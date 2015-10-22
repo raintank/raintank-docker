c0af99158d8f903b992495f3cffd1cb92e0d555f
Dieter Plaetinck <dieter@raintank.io>
2015-10-22 17:32:06 -0400

    measure grafana

--- measure.sh
+++ measure.sh
@@ -4,6 +4,8 @@ while true; do
   echo "$(date) measuring.."
   c_cpu=$(ps aux | grep -v awk | awk '/java.*cassandra/ { print $3}')
   c_rss=$(ps aux | grep -v awk | awk '/java.*cassandra/ { print $6}')
+  g_cpu=$(ps aux | grep -v awk | awk '/grafana-server/ { print $3}')
+  g_rss=$(ps aux | grep -v awk | awk '/grafana-server/ { print $6}')
   kairos_cpu=$(ps aux | grep -v awk | awk '/java.*kairosdb/ {print $3}')
   kairos_rss=$(ps aux | grep -v awk | awk '/java.*kairosdb/ {print $6}')
   nmt_cpu=$(ps aux | grep -v awk | awk '/nsq_metrics_tank/ {print $3}')
@@ -14,6 +16,8 @@ while true; do
 cat << EOF | nc -c localhost 2003
 measure.cassandra_cpu $c_cpu $ts
 measure.cassandra_rss $c_rss $ts
+measure.grafana_cpu $g_cpu $ts
+measure.grafana_rss $g_rss $ts
 measure.kairosdb_cpu $kairos_cpu $ts
 measure.kairosdb_rss $kairos_rss $ts
 measure.nmt_cpu $nmt_cpu $ts
