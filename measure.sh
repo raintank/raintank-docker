0086427ea390b5ea7110e6e9edbcffdf8e04e417
Dieter Plaetinck <dieter@raintank.io>
2015-10-25 13:30:34 -0400

    graphite-api stats

--- measure.sh
+++ measure.sh
@@ -13,7 +13,7 @@ while true; do
   nmk_cpu=$(ps aux | grep -v awk | awk '/nsq_metrics_to_kairos/ {print $3}')
   nmk_rss=$(ps aux | grep -v awk | awk '/nsq_metrics_to_kairos/ {print $6}')
   ts=$(date +%s)
-cat << EOF | nc -c localhost 2003
+  ( cat << EOF
 measure.cassandra_cpu $c_cpu $ts
 measure.cassandra_rss $c_rss $ts
 measure.grafana_cpu $g_cpu $ts
@@ -25,5 +25,8 @@ measure.nmt_rss $nmt_rss $ts
 measure.nmk_cpu $nmk_cpu $ts
 measure.nmk_rss $nmk_rss $ts
 EOF
+ps aux | grep -v awk | awk 'BEGIN { worker = 0} /python.*gunicorn/ {worker++; print "measure.graphite-api." worker "_cpu", $3}' | sed "s#\$# $ts#"
+ps aux | grep -v awk | awk 'BEGIN { worker = 0} /python.*gunicorn/ {worker++; print "measure.graphite-api." worker "_rss", $6}' | sed "s#\$# $ts#"
+) | nc -c localhost 2003
   sleep 10
 done
