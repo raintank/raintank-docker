7c48eb857d39ba14c1157941eb735a8ccb0b6658
Dieter Plaetinck <dieter@raintank.io>
2015-10-27 15:52:41 -0400

    do screen commands initialisation more elegantly
    i was getting fed up with the mess in launch.sh
    this is much cleaner and more consistent

--- launch_dev.sh
+++ launch_dev.sh
@@ -28,52 +28,19 @@ while [ $(docker ps | grep -c raintankdocker) -ne $num ]; do
   echo "waiting for all $num containers to run..."
   sleep 0.5
 done
-sleep 10
-echo "opening screen tabs..."
+echo "starting screen tabs..."
+for service in screens/*; do
+  base=$(basename $service)
+  if [ $base == measure ]; then
+    screen -S raintank -X screen -t $base bash
+  else
+   screen -S raintank -X screen -t $base docker exec -t -i raintankdocker_${base}_1 bash
+  fi
+  while read line; do
+    screen -S raintank -p $(basename $service) -X stuff "$line\n"
+  done < $service
+done
 
-screen -S raintank -X screen -t graphite-api docker exec -t -i raintankdocker_graphiteApi_1 bash
-screen -S raintank -X screen -t grafana docker exec -t -i raintankdocker_grafana_1 bash
-screen -S raintank -X screen -t statsdaemon docker exec -t -i raintankdocker_statsdaemon_1 bash
-screen -S raintank -X screen -t influxdb docker exec -t -i raintankdocker_influxdb_1 bash
-screen -S raintank -X screen -t kairosdb docker exec -t -i raintankdocker_kairosdb_1 bash
-screen -S raintank -X screen -t mysql-cli docker exec -t -i raintankdocker_mysql_1 bash
-screen -S raintank -X screen -t graphite_watcher docker exec -t -i raintankdocker_graphitewatcher_1 bash
-screen -S raintank -X screen -t nsq_metrics_tank docker exec -t -i raintankdocker_nsqmetricstank_1 bash
-screen -S raintank -X screen -t nsq_metrics_to_stdout docker exec -t -i raintankdocker_nsqmetricstostdout_1 bash
-screen -S raintank -X screen -t nsq_metrics_to_elasticsearch docker exec -t -i raintankdocker_nsqmetricstoelasticsearch_1 bash
-screen -S raintank -X screen -t nsq_probe_events_to_elasticsearch docker exec -t -i raintankdocker_nsqprobeeventstoelasticsearch_1 bash
-screen -S raintank -X screen -t measure bash
-screen -S raintank -X screen -t benchmark docker exec -t -i raintankdocker_benchmark_1 bash
-
-echo "starting commands in screen tabs..."
-screen -S raintank -p graphite-api -X stuff 'tail -10f /var/log/raintank/graphite-api.log\n'
-screen -S raintank -p grafana -X stuff '/tmp/create-influxdb-dev-datasource.sh &> /var/log/raintank/create-influxdb-datasource.log;\n'
-screen -S raintank -p grafana -X stuff '/tmp/prepare-nsqd.sh &> /var/log/raintank/prepare-nsqd.log;\n'
-screen -S raintank -p grafana -X stuff 'touch /var/log/raintank/grafana-dev.log\n'
-screen -S raintank -p grafana -X stuff 'cat /var/log/raintank/create-influxdb-datasource.log\n'
-screen -S raintank -p grafana -X stuff 'tail -10f /var/log/raintank/grafana-dev.log\n'
-screen -S raintank -p kairosdb -X stuff 'tail -f /opt/kairosdb/log/kairosdb.log\n'
-screen -S raintank -p mysql-cli -X stuff 'while sleep 1; do mysql -prootpass grafana; done\n'
-screen -S raintank -p graphite_watcher -X stuff 'touch /var/log/raintank/graphite-watcher.log\n'
-screen -S raintank -p graphite_watcher -X stuff 'cd /go/src/github.com/raintank/raintank-metric/graphite-watcher\n'
-screen -S raintank -p graphite_watcher -X stuff '/wait.sh elasticsearch:9200 influxdb:2003 graphite-api:8888 && ./graphite-watcher elasticsearch:9200 influxdb:2003 graphite-api:8888 &> /var/log/raintank/graphite-watcher.log &\n'
-screen -S raintank -p graphite_watcher -X stuff 'tail -f /var/log/raintank/graphite-watcher.log\n'
-screen -S raintank -p nsq_metrics_tank -X stuff 'cd /go/src/github.com/raintank/raintank-metric/nsq_metrics_tank\n'
-screen -S raintank -p nsq_metrics_tank -X stuff '/wait.sh cassandra:9042 && ./nsq_metrics_tank --chunkspan 120 --numchunks 5 --statsd-addr statsdaemon:8125 --nsqd-tcp-address nsqd:4150 --listen :6063 --cassandra-addrs cassandra &> /var/log/raintank/nsq_metrics_tank.log &\n'
-screen -S raintank -p nsq_metrics_tank -X stuff 'tail -f /var/log/raintank/nsq_metrics_tank.log\n'
-screen -S raintank -p nsq_metrics_to_stdout -X stuff 'cd /go/src/github.com/raintank/raintank-metric/nsq_metrics_to_stdout\n'
-screen -S raintank -p nsq_metrics_to_stdout -X stuff './nsq_metrics_to_stdout --nsqd-tcp-address nsqd:4150\n'
-screen -S raintank -p nsq_metrics_to_elasticsearch -X stuff 'cd /go/src/github.com/raintank/raintank-metric/nsq_metrics_to_elasticsearch\n'
-screen -S raintank -p nsq_metrics_to_elasticsearch -X stuff '/wait.sh elasticsearch:9200 redis:6379\n'
-screen -S raintank -p nsq_metrics_to_elasticsearch -X stuff './nsq_metrics_to_elasticsearch --elastic-addr elasticsearch:9200 --redis-addr redis:6379 --statsd-addr statsdaemon:8125 --nsqd-tcp-address nsqd:4150 &> /var/log/raintank/nsq_metrics_to_elasticsearch.log &\n'
-screen -S raintank -p nsq_metrics_to_elasticsearch -X stuff 'tail -f /var/log/raintank/nsq_metrics_to_elasticsearch.log\n'
-screen -S raintank -p nsq_probe_events_to_elasticsearch -X stuff 'cd /go/src/github.com/raintank/raintank-metric/nsq_probe_events_to_elasticsearch\n'
-screen -S raintank -p nsq_probe_events_to_elasticsearch -X stuff '/wait.sh elasticsearch:9200\n'
-screen -S raintank -p nsq_probe_events_to_elasticsearch -X stuff './nsq_probe_events_to_elasticsearch --elastic-addr elasticsearch:9200 --statsd-addr statsdaemon:8125 --nsqd-tcp-address nsqd:4150 &> /var/log/raintank/nsq_probe_events_to_elasticsearch.log &\n'
-screen -S raintank -p nsq_probe_events_to_elasticsearch -X stuff 'tail -f /var/log/raintank/nsq_probe_events_to_elasticsearch.log\n'
-screen -S raintank -p statsdaemon -X stuff 'tail -f /var/log/statsdaemon.log\n'
-screen -S raintank -p influxdb -X stuff 'tail -f /opt/influxdb/shared/log.txt\n'
-screen -S raintank -p measure -X stuff './measure.sh\n'
 
 ./wait.sh localhost:8086
 curl -X POST "localhost:8086/db/raintank/series?u=graphite&p=graphite" -d '[{"name": "events","columns": ["type","tags","text"],"points": [["devstack-start", "start", "devstack started"]]}]'
