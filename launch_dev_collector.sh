0f801e69b1f7a27e275327cdbceb9404e4a88575
Dieter Plaetinck <dieter@raintank.io>
2015-11-23 20:07:07 +1300

    echo right after curl whose output doesn't have a newline

--- launch_dev_collector.sh
+++ launch_dev_collector.sh
@@ -35,10 +35,10 @@ done
 mysql_id=$(sed 's#.*"id":\([0-9]\+\),"org_id":[0-9]\+,"slug":"'$id'".*#\1#' <<< "$data")
 # make it a "public" collector so different orgs can use it
 curl -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -F "public=true" -F "enabled=true" -F "name=$id" -F "id=$mysql_id" 'http://localhost/api/collectors'
+echo
 
 # restart the collector after making it public.
 screen -S raintank -p collector-$id -X stuff 'supervisorctl stop all && pkill -f -9 raintank-probe && supervisorctl start all\n'
 screen -S raintank -p collector-$id -X stuff 'tail -10f /var/log/raintank/collector.log\n'
 # seeing restarts happening before the DB is updated. so lets add a delay here.
 sleep 5
-echo
