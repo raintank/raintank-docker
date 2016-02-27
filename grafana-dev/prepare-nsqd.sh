ef0d07b11f68cc18633311f3ee03b963a80446a1
Dieter Plaetinck <dieter@raintank.io>
2016-02-27 11:23:19 +1100

    metrics->ES is handled by MT for a while now

--- grafana-dev/prepare-nsqd.sh
+++ grafana-dev/prepare-nsqd.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 curl -X POST 'http://nsqd:4151/topic/create?topic=metrics'
 curl -X POST 'http://nsqd:4151/channel/create?topic=metrics&channel=tank'
-curl -X POST 'http://nsqd:4151/channel/create?topic=metrics&channel=elasticsearch'
