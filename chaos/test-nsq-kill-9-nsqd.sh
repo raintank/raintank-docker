cef006130fbd2614b2c296914e2540c46770938c
Dieter Plaetinck <dieter@raintank.io>
2016-03-01 21:58:24 +1100

    move chaos scripts into separate directory

--- chaos/test-nsq-kill-9-nsqd.sh
+++ chaos/test-nsq-kill-9-nsqd.sh
@@ -5,7 +5,7 @@ function log () {
 }
 
 log "STEP 1. starting stack"
-./launch_dev.sh
+../launch_dev.sh
 log "stack is launched."
 
 log "STEP 2. waiting for grafana to compile and run.."
@@ -18,7 +18,7 @@ pgrep -fl grafana-server
 pgrep -fl nsq_to_kairos
 log "now waiting for grafana to start publishing.."
 log "open http://localhost:4171/topic/metrics to monitor state"
-while ! docker exec raintankdocker_grafana_1 grep -q 'published metrics' /var/log/raintank/grafana-dev.log; do
+while ! docker exec raintankdocker_grafana_1 grep -q PUBLISH /var/log/raintank/grafana-dev.log; do
   sleep 1
 done
 
@@ -26,14 +26,10 @@ log "STEP 4. publishing started"
 log "let it running for 1 minute.."
 sleep 60
 
-log "STEP 5. kill nsq-to-kairos"
-docker exec raintankdocker_nsqtokairos_1 pkill -f nsq_to_kairos
-log "wait 5 minutes"
-sleep 300
-
-log "STEP 6. restarting nsq_to_kairos"
-screen -S raintank -p nsqtokairos -X stuff './nsq_to_kairos --topic metrics --channel tokairos --nsqd-tcp-address nsqd:4150 2>&1 | tee /var/log/raintank/nsq-to-kairos2.log\n'
-log "wait another minute..."
+log "STEP 5. kill -9 nsqd"
+pid=$(docker exec raintankdocker_nsqd_1 ps aux | grep /nsqd | grep -v while | awk '{print $1}')
+docker exec raintankdocker_nsqd_1 kill -9 $pid
+log "wait a minute..."
 sleep 60
 
 log "STEP 7. stop grafana aka producer"
@@ -47,4 +43,4 @@ read
 
 log "STEP 8. killing nsq_to_kairos as well, again"
 docker exec raintankdocker_nsqtokairos_1 pkill -f nsq_to_kairos
-log "STEP 9. have fun disecting the logs"
+log "STEP 9. have fun disecting the logs. at most 1 message should be lost"
