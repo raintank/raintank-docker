328ba82461243feb1bfe414d8da9b309f089feea
woodsaj <anthony@monkey.id.au>
2016-03-10 15:07:00 +0800

    get everything working.

--- scripts/dashboards/create-datasource.sh
+++ scripts/dashboards/create-datasource.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 BASE=$(dirname $0)
+sleep 30
+
 echo "> adding datasources"
 
 curl -u admin:admin \
@@ -9,6 +11,7 @@ curl -u admin:admin \
 curl -u admin:admin \
   -H "content-type: application/json" \
   'http://localhost/api/datasources' -X POST --data-binary '{"name":"benchmarks","type":"elasticsearch","url":"http://elasticsearch:9200","access":"proxy","isDefault":false,"database":"benchmark","user":"","password":"", "jsonData": {"timeField": "timestamp"}}'
+
 
 for file in $BASE/*.json; do
   echo "> adding dashboard $file"
