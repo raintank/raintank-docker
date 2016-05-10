7c3637bd234f0d51867ef523ebcf37af6377b4b2
Dieter Plaetinck <dieter@raintank.io>
2016-05-10 11:13:37 -0400

    make monitoring easier

--- measure.sh
+++ measure.sh
@@ -5,9 +5,7 @@ COLUMNS=512 top -b -c | grep -v sed | sed -u -n \
   -e 's#`-.*java.*elasticsearch.*#elasticsearch#p' \
   -e 's#`-.*grafana-server.*#grafana#p' \
   -e 's#`- /nsqd .*#nsqd#p' \
-  -e 's#`- ./metric_tank.*#nmt#p' \
-  -e 's#`- ./nsq_metrics_to_kairos.*#nmk#p' \
-  -e 's#`- ./nsq_metrics_to_elastic.*#nme#p' \
+  -e 's#`- ./metric_tank.*#metric-tank#p' \
   -e 's#`- ./nsq_probe_events_to_elastic.*#npee#p' \
   -e 's#`-.*python.*gunicorn.*#graphite-api#p' \
   -e 's#`-.*bin/carbon-relay-ng proxy.ini.*#carbon-relay-ng#p' \
@@ -20,16 +18,16 @@ COLUMNS=512 top -b -c | grep -v sed | sed -u -n \
     mem=${mem/m/}
     if [ "$process" == "graphite-api" ]; then
       graphite_api_i=$((graphite_api_i + 1))
-      echo "measure.${process}.${graphite_api_i}_cpu $cpu $ts"
-      echo "measure.${process}.${graphite_api_i}_rss $mem $ts"
+      echo "measure.${process}.${graphite_api_i}.cpu $cpu $ts"
+      echo "measure.${process}.${graphite_api_i}.rss $mem $ts"
     elif [ "$process" == "collector" ]; then
       collector_i=$((collector_i + 1))
-      echo "measure.${process}.${collector_i}_cpu $cpu $ts"
-      echo "measure.${process}.${collector_i}_rss $mem $ts"
+      echo "measure.${process}.${collector_i}.cpu $cpu $ts"
+      echo "measure.${process}.${collector_i}.rss $mem $ts"
     else
       graphite_api_i=0
       collector_i=0
-      echo "measure.${process}_cpu $cpu $ts"
-      echo "measure.${process}_rss $mem $ts"
+      echo "measure.${process}.cpu $cpu $ts"
+      echo "measure.${process}.rss $mem $ts"
     fi
 done | nc -c localhost 2003
