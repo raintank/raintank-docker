c4ea508890b40beee65721704cd56f19bae7b18c
Dieter Plaetinck <dieter@raintank.io>
2016-08-26 16:28:58 +0300

    pull in new plugin code too
    (note: grafana container doesn't actually use the code, I think)

--- setup_dev.sh
+++ setup_dev.sh
@@ -21,7 +21,7 @@ if [ "$MODE" == "docker" ]; then
 
 	args=("${args[@]}" "-v" "$DIR:/opt/raintank/raintank-docker")
 	# assure the directories exist (irrespective of what we'll do with them, see below) so we can set up the volumes
-	for i in inspect fakemetrics metrictank eventtank worldping-api raintank-probe raintank-apps carbon-relay-ng tsdb-gw; do
+	for i in inspect fakemetrics metrictank eventtank worldping-api raintank-probe raintank-apps carbon-relay-ng tsdb-gw plugins/worldping-app; do
 		mkdir -p $i
 		args=("${args[@]}" "-v" "$RT_CODE/$i:/go/src/github.com/raintank/$i")
 	done
@@ -34,7 +34,7 @@ if [ "$MODE" == "docker" ]; then
 
 elif [ $MODE == "code" ]; then
 	cd /go/src/github.com/raintank
-	for i in inspect fakemetrics raintank-collector metrictank eventtank worldping-api raintank-probe raintank-apps carbon-relay-ng tsdb-gw; do
+	for i in inspect fakemetrics raintank-collector metrictank eventtank worldping-api raintank-probe raintank-apps carbon-relay-ng tsdb-gw plugins/worldping-app; do
 		echo "> processing code for $i"
 		if [ -f /go/src/github.com/raintank/$i/.notouch ]; then
 			echo "Skipping due to .notouch"
