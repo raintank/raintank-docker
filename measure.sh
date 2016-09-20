b84852c578c78fa17eb811333a923a1c01d6daeb
Dieter Plaetinck <dieter@raintank.io>
2016-09-20 11:32:22 +0300

    fix MT monitoring
    measure all instances

--- measure.sh
+++ measure.sh
@@ -5,7 +5,7 @@ COLUMNS=512 top -b -c | grep -v sed | sed -u -n \
   -e 's#`-.*java.*elasticsearch.*#elasticsearch#p' \
   -e 's#`-.*grafana-server.*#grafana#p' \
   -e 's#`- /nsqd .*#nsqd#p' \
-  -e 's#`- ./metrictank.*#metrictank#p' \
+  -e 's#`- /usr/bin/metrictank.*#metrictank#p' \
   -e 's#`- ./nsq_probe_events_to_elastic.*#npee#p' \
   -e 's#`-.*python.*gunicorn.*#graphite-api#p' \
   -e 's#`-.*/carbon-relay-ng.*proxy.ini$#carbon-relay-ng#p' \
@@ -20,6 +20,14 @@ COLUMNS=512 top -b -c | grep -v sed | sed -u -n \
   | awk '{print $6,$7,$11;fflush();}' \
   | while read mem cpu process; do
     ts=$(date +%s)
+    if [ "$ts" != "$prevTs" ]; then
+      # a bit hacky.. since we receive a stream of process measurements, where multiple graphite instances that need sequential numbering
+      # are intermingled with updates for the next time interval, we need a way to figure out when to bump the number vs resetting it.
+      # checking the timestamps of our parse loop is not rock solid but should be good enough
+      graphite_api_i=0
+      collector_i=0
+      mt_i=0
+    fi
     if [[ $mem =~ g$ ]]; then
 	    mem=$(bc -l <<< "${mem:0:-1} * 1024 * 1024 * 1024")
     elif [[ $mem =~ m$ ]]; then
@@ -36,10 +44,13 @@ COLUMNS=512 top -b -c | grep -v sed | sed -u -n \
       collector_i=$((collector_i + 1))
       echo "measure.${process}.${collector_i}.cpu $cpu $ts"
       echo "measure.${process}.${collector_i}.rss $mem $ts"
+    elif [ "$process" == "metrictank" ]; then
+      mt_i=$((mt_i + 1))
+      echo "measure.${process}.${mt_i}.cpu $cpu $ts"
+      echo "measure.${process}.${mt_i}.rss $mem $ts"
     else
-      graphite_api_i=0
-      collector_i=0
       echo "measure.${process}.cpu $cpu $ts"
       echo "measure.${process}.rss $mem $ts"
     fi
+    prevTs=$ts
 done | nc -c localhost 2003
