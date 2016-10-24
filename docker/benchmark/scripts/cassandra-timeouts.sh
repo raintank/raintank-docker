38539947132d43990e4ac14e29029c359020d582
Dieter Plaetinck <dieter@raintank.io>
2016-10-24 16:06:32 +0300

    run simulation controlled entirely automatically from benchmark script
    * move toxiproxy-cli to benchmark container (requires you have a symlink
      in code to the toxiproxy checkout that has a compiled toxyproxy-cli !!)
    * move the simulation to a standalone script, since the
      screens/benchmark is becoming a huge mess.

---
+++ docker/benchmark/scripts/cassandra-timeouts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# first steps:
+# in ./code, need metrictank latest cassandra-retry branch
+# launch docker stack, make sure all containers are running with docker ps -a
+# go to http://localhost:3000/plugins/raintank-worldping-app/edit to login and enter `changeme` to have plugin create metrictank datasource
+# go to http://localhost:3000/dashboard/db/fake-metrics-data and change the query to only show 1 or 2 metrics, which is easier to see. 
+# reload that dash a couple times both during the HEALTH and the TIMEOUTS step of this simulation
+
+# we wait for metrictank even though fakemetrics doesn't connect to it,
+# but since it uses latest offset we need this for the backfill+realtime load to be correct, and also vegeta start
+wait.sh kafka:9092 metrictank:6063
+cd /go/src/github.com/raintank/fakemetrics
+
+# fill up all of metrictanks RAM followed by a realtime load
+./fakemetrics -listen :6764 -kafka-mdm-tcp-address kafka:9092 -kafka-comp none -statsd-addr statsdaemon:8125 -orgs 10 -keys-per-org 100 -speedup 30 -offset 90min -stop-at-now
+./fakemetrics -listen :6764 -kafka-mdm-tcp-address kafka:9092 -kafka-comp none -statsd-addr statsdaemon:8125 -orgs 10 -keys-per-org 100 &
+
+# let the "realtime workload" settle in for a bit, and measure how MT performs
+sleep 10s
+echo "HEALTHY:"
+inspect-idx -from 1h cass cassandra:9042 raintank vegeta-mt | vegeta attack -rate 10 -duration 60s > vegeta-healthy
+cat vegeta-healthy | vegeta report
+
+# make cassandra timeout once in a while, and measure how MT performs
+/go/src/github.com/Shopify/toxiproxy/cmd/toxiproxy-cli/toxiproxy-cli -h http://toxiproxy:8474 toxic add -t latency -a latency=0 -a jitter=1500 cassandra
+echo "TIMEOUTS:"
+inspect-idx -from 1h cass cassandra:9042 raintank vegeta-mt | vegeta attack -rate 10 -duration 60s > vegeta-timeouts
+cat vegeta-timeouts | vegeta report
