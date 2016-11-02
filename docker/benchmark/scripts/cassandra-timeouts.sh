67f2a14394bf0ef1b91b246051644ee473929d80
Dieter Plaetinck <dieter@raintank.io>
2016-11-02 14:14:04 +0200

    make the cassandra timeouts simulation an optional thing
    so that we can merge this into master.
    This reverts commit e1ae4d85e7053c48244f12cf6be1bd196ac9b3ba.

--- docker/benchmark/scripts/cassandra-timeouts.sh
+++ docker/benchmark/scripts/cassandra-timeouts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # first steps:
-# in ./code, need metrictank latest cassandra-retry branch
+# edit the MT config so it has no aggregations, and make sure it keeps a small amount of data in ram (e.g. 2x10min chunks)
 # launch docker stack, make sure all containers are running with docker ps -a
 # go to http://localhost:3000/plugins/raintank-worldping-app/edit to login and enter `changeme` to have plugin create metrictank datasource
 # open metrictank dash, and sys dash and take snapshot at the end.
-# optional: open fake metrics data dash and reload a couple times both during the HEALTH and the TIMEOUTS step of this simulation to see if data is missing
+# optional: open fake metrics data dash, change the query to show only 1 or 2 metrics, which is easier to see, and reload a couple times both during the HEALTH and the TIMEOUTS step of this simulation to see if data is missing
 
 # we wait for metrictank even though fakemetrics doesn't connect to it,
 # but since it uses latest offset we need this for the backfill+realtime load to be correct, and also vegeta start
